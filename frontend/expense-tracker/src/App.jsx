12fc3e1f64cde06db792a7026260d7d58fa76e00
Sagnik Pal <131534396+SagnikPal-7@users.noreply.github.com>
2025-05-13 14:48:11 +0530

    test2

--- frontend/expense-tracker/src/App.jsx
+++ frontend/expense-tracker/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 import {
   BrowserRouter as Router,
